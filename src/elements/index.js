0ca5a626e12d5225d1781cfacd2e4dd78515c728
unknown <isabelrlewis@gmail.com>
2019-11-10 23:49:00 +0000

    new build for prod

--- src/elements/index.js
+++ src/elements/index.js
@@ -4,4 +4,3 @@ export { SectionHeading } from "./section-heading/SectionHeading";
 export { ShortCopy } from "./short-copy/ShortCopy";
 export { Link } from "./link/Link";
 export { Logo } from "./logo/Logo";
-export { Card } from "./card/Card";
